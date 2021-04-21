76af08e1b1f4372733476ca70dbada2d8ab642a8
pantazheng <pantazheng@vip.qq.com>
2021-04-21 13:36:08 +0800

    提取存储展示

--- getstore.sh
+++ getstore.sh
@@ -7,17 +7,18 @@ fi
 for i in $(seq 1 3)
 do
     echo "orderer$i.example.com" >> $ordererfile
-    docker exec -ti orderer$i.example.com du -h /var/hyperledger/production >> $ordererfile
+    du -h vars/store/orderers/orderer$i.example.com >> $ordererfile
 done
 
 org0file="org0-storage.log"
 if [ -f $org0file ]; then
     rm $org0file
 fi
+
 for i in $(seq 1 2)
 do
     echo "peer$i.org0.example.com" >> $org0file
-    docker exec -ti peer$i.org0.example.com du -h /var/hyperledger/production >> $org0file
+    du -h vars/store/peers/peer$i.org0.example.com >> $org0file
 done
 
 org1file="org1-storage.log"
@@ -27,5 +28,5 @@ fi
 for i in $(seq 1 4)
 do
     echo "peer$i.org1.example.com" >> $org1file
-    docker exec -ti peer$i.org1.example.com du -h /var/hyperledger/production >> $org1file
+    du -h vars/store/peers/peer$i.org1.example.com >> $org1file
 done
